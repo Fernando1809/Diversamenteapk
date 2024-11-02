fc178eccefbe965142a2ec364f869a8c8464e890
wgutierrez <adolfoguti001@outlook.com>
2024-11-01 23:13:19 -0600

    feat: login is done by password and email..

--- Diversamente/app/src/main/java/com/example/diversamente/MainActivity.java
+++ Diversamente/app/src/main/java/com/example/diversamente/MainActivity.java
@@ -1,15 +1,31 @@
 package com.example.diversamente;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class MainActivity extends AppCompatActivity {
     private FirebaseAuth mAuth;
+    private EditText txtEmail, txtPassword, txtuserName;
+    private Button btnLogin, btnSignUp;
+    private TextView registerNowBtn;
+    private DatabaseReference databaseReference, userReference;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,11 +34,58 @@ public class MainActivity extends AppCompatActivity {
 
         mAuth = FirebaseAuth.getInstance();
 
-        Button btnSignUp = findViewById(R.id.btnSignUp);
+        databaseReference = FirebaseDatabase.getInstance().getReferenceFromUrl("https://diversamente-1d32d-default-rtdb.firebaseio.com/");
+
+        txtEmail = findViewById(R.id.txtEmail);
+        txtPassword = findViewById(R.id.txtPassword);
+        final Button btnLogin = findViewById(R.id.btnLogin);
+        btnSignUp = findViewById(R.id.btnSignUp);
+
         btnSignUp.setOnClickListener(view -> {
-            // Iniciar la actividad de registro
             Intent intent = new Intent(getApplicationContext(), SignUpActivity.class);
             startActivity(intent);
         });
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                login();
+
+            }
+        });
+    }
+
+    private void login (){
+        String email = txtEmail.getText().toString();
+        String password = txtPassword.getText().toString();
+
+        if (email.isEmpty() || password.isEmpty()) {
+            MsgToast("Por favor completa los campos");
+            return;
+        }
+
+        mAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        try {
+                            if (task.isSuccessful()) {
+                                // Implementa la lógica para redirigir al usuario a la actividad principal
+                                startActivity(new Intent(getApplicationContext(), HomeActivity.class));
+                                finish();
+
+                            } else {
+                                MsgToast("Error en el inicio de sesión");
+                            }
+                        }catch (Exception e){
+                            MsgToast(e.getMessage());
+                        }
+
+                    }
+                });
+
+    }
+    private void MsgToast(String msg){
+        Toast.makeText(getApplicationContext(),msg, Toast.LENGTH_LONG).show();
     }
 }
